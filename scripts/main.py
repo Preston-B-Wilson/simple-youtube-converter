f959aeed60fb651b024bf15be0b53e50f1d3cd99
YoSoyPan <45904881+yosoypan@users.noreply.github.com>
2022-02-10 14:08:21 -0600

    ui improvments

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,4 @@
+import os
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import StringProperty
@@ -7,14 +8,48 @@ from kivy.uix.widget import Widget
 
 class MainScreen(GridLayout):
 
-    text_input_str = StringProperty("")
+    cwd = os.getcwd()
+
+    last_text_input = ""
+    all_text_input = []
+    kivy_text_output = StringProperty("")
+
+    current_dl_dir = cwd
+    kivy_dir_output = StringProperty(cwd)
 
     def delete_queue(self):
-        self.text_input_str = ""
+        self.last_text_input = ""
+        self.all_text_input = []
+        self.kivy_text_output = ""
 
     def add_to_queue(self):
-        self.text_input_str = self.ids.my_text_input.text
-        self.ids.my_text_input.text = ""
+        input_txt = self.ids.my_text_input
+
+        if input_txt.text == "":
+            self.ids.url_warning.text = "Cannot enter empty url"
+            return
+        else:
+            self.ids.url_warning.text = ""
+
+        self.last_text_input = input_txt.text
+        input_txt.text = ""
+
+        self.all_text_input.append(self.last_text_input)
+        reconstructed_text = ""
+
+        for url in self.all_text_input:
+            reconstructed_text += url + "\n"
+
+        self.kivy_text_output = reconstructed_text
+
+    def update_dl_dir(self):
+        self.current_dl_dir = self.ids.my_text_input2.text
+        self.ids.my_text_input2.text = ""
+        self.kivy_dir_output = self.current_dl_dir
+
+    def revert_dl_dir(self):
+        self.current_dl_dir = self.cwd
+        self.kivy_dir_output = self.cwd 
 
 class SimpleYTConverterApp(App):
     pass
